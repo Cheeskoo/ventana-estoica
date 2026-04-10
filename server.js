5ae2e774bf69e346afbaa7d3c1bbf9ef0ac1a943
Cheeskoo <159967078+Cheeskoo@users.noreply.github.com>
2026-04-10 00:50:55 -0400

    Update
    Armando api y estructura basica del documento html

---
+++ server.js
@@ -0,0 +1,37 @@
+import http from 'node:http';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { sendResponse } from './utils/sendResponse.js'
+
+const PORT = 3000;
+const __dirname = import.meta.dirname
+
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', "GET")
+
+    if (req.url === '/api/quote'){
+        try{ 
+        const route = path.join(__dirname, 'package.json')
+        const data = await fs.readFile(route)
+        const quotes = JSON.parse(data)
+        
+        const randomIndex = Math.floor(Math.random() * quotes.length)
+        const randomQuote = quotes[randomIndex]
+        
+        sendResponse(res, 201, 'application/json', randomQuote)
+
+        }catch(error){
+            console.log(`error al leer las frases : ${error}`)
+            sendResponse(res, 500, 'application/json', {
+                "message": "Error servidor 500"
+            })
+            
+        }
+
+    }else if(!req.url.startsWith('/')){
+        return await serveStatic
+    }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
---
+++ utils/serveStatic.js
@@ -0,0 +1,31 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { getContentType } from './contentType.js'
+import { sendResponse } from './sendResponse.js'
+
+export async function serveStatic(req, res, basedir){
+    const publicDir = path.join(basedir, 'public')
+    const filePath = path.join(publicDir, req.url === '/' ? 'index.html': req.url)
+    
+    const ext = path.extname(filePath)
+
+    const contentType = getContentType(ext)
+    try{
+        const data = await fs.readFile(filePath)
+        sendResponse(res, 200, contentType, data)
+    }catch(error){
+
+        if (error.code === 'ENOENT'){ 
+            console.log(`error: ${error}`)
+            sendResponse(res, 404, 'application/json', {
+                "message": "404 Not Found"
+        })
+        }else{
+            sendResponse(res, 500, 'application/json', {
+            "message": "500 Server failed"
+        })
+                                    }
+
+                }
+    
+}
